f59b61b5abf3a4935c5a02b97050eed4c6ac34c3
hyunsoo <hjames2000@snu.ac.kr>
2023-10-31 10:59:11 +0900

    fd

--- Graph.java
+++ Graph.java
@@ -1,22 +1,33 @@
 public class Graph{
     private int numVertex = 0;
     private int numEdge = 0;
-    private Vertex[] listVertex;
-    private Edge[] listEdge;
-    private int[][] adjacentMatrix;
+    private Vertex[] listVertex = new Vertex[1000];
+    private Edge[] listEdge = new Edge[1000000];
+    private int[][] adjacentMatrix = new int[1000][1000];
+    private LinkedList[] adjacentList = new LinkedList[1000];
     
     public class LinkedList{
-        int key = 0;
+        Vertex key;
+        LinkedList next;
         
+        public LinkedList(Vertex key_){
+            key = key_;
+            next = null;
+        }
     }
 
     public class Vertex{
         int key;
+        String name;
         boolean connected;
 
-        public Vertex(int key_){
+        public Vertex(int key_, String name_){
+            name = name_;
             key = key_;
             connected = false;
+        }
+        public String getName(){
+            return name;
         }
     }
 
@@ -34,6 +45,25 @@ public class Graph{
     }
 
     public Graph(){
+
+    }
+
+    public boolean isValid(String name){
+        for(int i = 0; i < numVertex; i++){
+            if(name.equals(listVertex[i].getName())) return false;
+        }
+        return true;
+    }
+
+    public void newVertex(String name){
+        if(numVertex > 999){
+            System.out.println("Too much vertices.");
+            return;
+        }
+        if(!isValid(name)){
+            System.out.println("There already exists a vertex with same name.");
+        }
+        Vertex v = new Vertex(numVertex, name);
 
     }
 
