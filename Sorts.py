a56ce091b1b73a006dc688f2a7074635d1ae7f6e
hyunsoo <hjames2000@snu.ac.kr>
2023-11-22 19:07:14 +0900

    f

---
+++ Sorts.py
@@ -0,0 +1,67 @@
+import random
+
+### 오름차순 정렬
+
+A = [i for i in range(10000)]
+SIZE = 10000
+
+######### Sub functions #######
+
+def isSorted():
+  for i in range(SIZE-1):
+    if(A[i] >= A[i+1]):
+      return False
+  return True
+
+
+
+######### Sort functions #########
+
+## n^2
+
+def insertionSort():
+  cnt_swap = 0
+  cnt_compare = 0
+  for i in range(1,SIZE):
+    temp = A[i]
+    for j in range(i-1,-1,-1):
+      cnt_compare += 1
+      if(A[j] > temp):
+        cnt_swap+=1
+        A[j+1] = A[j]
+      else:
+        break
+    cnt_swap+=1
+    A[j+1] = temp
+  return cnt_swap, cnt_compare
+
+def selectionSort():
+  pass
+
+def bubbleSortNaive():
+  pass
+
+def bubbleSortImproved():
+  pass
+
+## n log(n)
+
+def quickSort():
+  pass
+
+def mergeSort():
+  pass
+
+def radixSort():
+  pass
+
+def heapSortNaive():
+  pass
+
+def heapSortInplace():
+  pass
+
+########## Main #########
+
+def main():
+  pass
